26fdeb179d8c711f43c37b8784af480e44c73b27
HamailSaleemi <49240407+HamailSaleemi@users.noreply.github.com>
2025-05-12 17:00:09 +0500

    login change to post api

--- routes/login_routes.py
+++ routes/login_routes.py
@@ -1,22 +1,28 @@
 from fastapi import APIRouter, HTTPException
 from DATABASE.connection import SQL_CONN
+from pydantic import BaseModel
 
 # Create an APIRouter instance
 router = APIRouter()
 
-@router.get("/")
-def login(username: str, password: str):
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+@router.post("/")
+def login(request: LoginRequest):
+    # return {"username": request.username, "password": request.password}
     try:
         query = "SELECT UserCode, username FROM Users WHERE username = ? AND password = ?"
-        params = (username, password)
+        params = (request.username, request.password)
         result = SQL_CONN.execute_query(query, params)
-
+        print('result is ',result)
         if not result:
             raise HTTPException(status_code=404, detail="User not found")
 
         # Convert result to a list of dictionaries
         users = [{"id": row[0], "username": row[1]} for row in result]
-
+        print('username ', users)
 
         return {"status": "success", "users": users}
 
